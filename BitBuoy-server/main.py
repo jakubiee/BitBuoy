55871edcc48827e14b5b6f23743cd451c9598be5
Jakub Klimkowski <sadgi.enjoyer@protonmail.com>
2024-10-30 02:33:40 +0100

    update server

--- BitBuoy-server/main.py
+++ BitBuoy-server/main.py
@@ -1,14 +1,15 @@
 import serial
 import serial.tools.list_ports
 import time
+import requests
+import json
+from datetime import datetime,timezone
 
 def list_usb_ports():
-    """List all available USB ports."""
     ports = serial.tools.list_ports.comports()
     return [port.device for port in ports]
 
 def select_usb_port():
-    """Allow the user to select a USB port with option to refresh."""
     while True:
         ports = list_usb_ports()
         
@@ -43,16 +44,50 @@ def select_usb_port():
                 print("Invalid input. Enter a number, 'r', or 'q'.")
 
 def serial_monitor(port_name):
-    """Connect to the selected USB port and print received data."""
     try:
         with serial.Serial(port_name, 9600, timeout=1) as ser:
             print(f"Connected to {port_name}. Press Ctrl+C to exit.")
             while True:
                 if ser.in_waiting > 0:
                     data = ser.readline().decode('utf-8').strip()
-                    print(data)
+                    data = data[10:]
+                    if data.startswith("ABCD1234"):
+                        send_measurement(data.split(","))
+
     except Exception as e:
         print(f"Error: {e}")
+
+
+
+def send_measurement(data):
+    url = 'https://bitbuoy-backend-production.up.railway.app/measurement/'
+    headers = {
+        'accept': 'application/json',
+        'token': 'securetoken123',
+        'Content-Type': 'application/json'
+    }
+    
+    data = {
+        "buoy_serial_number": data[0],
+        "ambient_temp": float(data[1]),
+        "water_temp": float(data[2]),
+        "water_pollution": float(data[3]),
+        "humidity": float(data[4]),
+        "lat": float(data[5]),
+        "long": float(data[6]),
+        "timestamp": datetime.now(timezone.utc).isoformat() 
+    }
+    
+    try:
+        response = requests.post(url, headers=headers, data=json.dumps(data))
+        response.raise_for_status() 
+        print("Request successful:", response.json())
+    except requests.exceptions.HTTPError as err:
+        print(f"HTTP error occurred: {err}")
+    except Exception as err:
+        print(f"Other error occurred: {err}")
+
+
 
 if __name__ == "__main__":
     selected_port = select_usb_port()
